89313b55c6f786b47175078f9564f2dc6823ba9d
x19990416 <starseeker.limin@gmail.com>
2016-05-11 13:02:00 +0800

    update with wechatServletModule

--- src/main/java/com/macrossx/application/Application.java
+++ src/main/java/com/macrossx/application/Application.java
@@ -19,22 +19,23 @@ import java.util.EventListener;
 import java.util.List;
 
 import com.google.common.collect.Lists;
-import com.google.inject.AbstractModule;
 import com.google.inject.Binder;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Module;
+import com.google.inject.name.Names;
 import com.macrossx.embedded.BootServer;
 import com.macrossx.embedded.ServletConfig;
 import com.macrossx.embedded.jetty.JettyServer;
 import com.macrossx.rest.HelloWorldResource;
-import com.macrossx.template.groovy.ITemplateGroovyHelper;
-import com.macrossx.template.groovy.TemplateGroovyHelper;
 import com.macrossx.template.groovy.TemplateGroovyModule;
+import com.macrossx.wechat.IWechatServer;
+import com.macrossx.wechat.WechatServletModule;
 import com.sun.jersey.guice.JerseyServletModule;
-import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
-	public class Application {
-	public static Injector  injector;
+
+public class Application {
+	public static Injector injector;
+
 	public static void main(String... s) {
 		Guice.createInjector(new Module() {
 			public void configure(Binder binder) {
@@ -42,16 +43,26 @@ import com.sun.jersey.guice.spi.container.servlet.GuiceContainer;
 				binder.bind(EventListener.class).toInstance(new ServletConfig() {
 					@Override
 					public List<Module> provider() {
-						return Lists.newArrayList(new TemplateGroovyModule(),new JerseyServletModule() {
+						return Lists.newArrayList(new Module() {
+
+							@Override
+							public void configure(Binder binder) {
+								binder.bindConstant().annotatedWith(Names.named("macrossx.wechat.token"))
+										.to("wechattest");
+								binder.bind(IWechatServer.class).to(WechatServer.class);
+							}
+						}, new TemplateGroovyModule(), new JerseyServletModule() {
 							protected void configureServlets() {
 								bind(HelloWorldResource.class);
-								serve("/*").with(GuiceContainer.class);
+								// serve("/*").with(GuiceContainer.class);
 							}
-						});
+						}, new WechatServletModule());
 					}
 				});
 			}
 		}).getInstance(BootServer.class).run();
 	}
+	
+	public static class WechatServer implements IWechatServer{}
 
 }
